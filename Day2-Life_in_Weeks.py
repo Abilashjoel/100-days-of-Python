7697e11032704b6bf294161f5780c74c1283f1f4
Abilash <40256764+Abilashjoel@users.noreply.github.com>
2021-03-17 14:51:15 +0530

    Add files via upload

---
+++ Day2-Life_in_Weeks.py
@@ -0,0 +1,7 @@
+age=int(input("What's Your Current Age "))
+yearsRemaining = 100- age
+daysRemaining = yearsRemaining * 365
+weeksRemaining = yearsRemaining * 52
+monthsRemaining = yearsRemaining * 12
+
+print(f"You have {daysRemaining} days , {weeksRemaining} weeks , {monthsRemaining}months , {yearsRemaining} years Remaining")
